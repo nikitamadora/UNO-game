9723111b12bf785b0c4c02a48a036d251fdc3c71
nmadora <nikitamadora@gmail.com>
2020-02-10 18:21:27 -0800

    Rename files for rebrand to stubborn-kittens

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log("sanity check");
+
+const game = {
+     #gameContainer {
+          width: 70%;
+          margin: 0 auto;
+          padding: 0% 5% 2.5% 5%;
+          border: 1px solid #E0E0E0;
+          background-color: #FFFFFF;
+          /* overflow-x: hidden; */
+        }
+};
